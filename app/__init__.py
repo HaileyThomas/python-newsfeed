1e65e1e792058c6c9847b9a38e66e9ba5a5a4413
HaileyThomas <haileyraebeauty@gmail.com>
2022-05-05 16:12:04 -0700

    set up homepage routes and filters

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
-#import routes
 from app.routes import home, dashboard
+from app.db import init_db
+from app.utils import filters
 
 # when flask runs the app package it will call a create_app() function
 
@@ -26,5 +26,13 @@ def create_app(test_config=None):
     # register routes
     app.register_blueprint(home)
     app.register_blueprint(dashboard)
+
+    # apply filters
+    app.jinja_env.filters["format_url"] = filters.format_url
+    app.jinja_env.filters["format_date"] = filters.format_date
+    app.jinja_env.filters["format_plural"] = filters.format_plural
+
+    # initialize database after app is ready
+    init_db(app)
 
     return app
--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template
+from app.models import Post
+from app.db import get_db
 
 # Blueprint lets us consolidate routes onto a single bp object that the parent app can register later
 # like the Router middleware in Express.js
@@ -9,7 +11,14 @@ bp = Blueprint("home", __name__, url_prefix="/")
 
 @bp.route("/")
 def index():
-    return render_template("homepage.html")
+    # get all posts
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+
+    return render_template(
+        "homepage.html",
+        posts=posts
+    )
 
 # define login route function
 
@@ -23,4 +32,12 @@ def login():
 
 @bp.route("/post/<id>")
 def single(id):
-    return render_template("single-post.html")
+    # get single post by id
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+
+    # render single post template
+    return render_template(
+        "single-post.html",
+        post=post
+    )
